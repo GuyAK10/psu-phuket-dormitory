1ab4985579335a25a04c140edfffa4fdec842026
GuyAK10 <akkaraphol.khunpet@gmail.com>
2020-10-25 17:27:14 +0700

    add get all student

--- backend/staff/profile.js
+++ backend/staff/profile.js
@@ -5,6 +5,26 @@ const router = express.Router()
 const db = firestore.firestore()
 const bucket = firestore.storage().bucket()
 
+router.get('/staff/profile/',  async (req, res) => {
+  try {
+      let studentList = []
+      const docRef = db.collection('students')
+      const profile = await docRef.get();
+      profile.forEach(list => {
+          let studentData = {
+              studentId: '',
+          }
+          studentData.studentId = list.id
+          Object.assign(studentData, list.data())
+          studentList.push(studentData)
+      })
+      res.status(200).send(studentList);
+  }
+  catch (error) {
+      res.sendStatus(500);
+  }
+});
+
 router.get('/staff/profile/:studentId', async (req, res) => {
   try {
     const studentId = req.params.studentId
