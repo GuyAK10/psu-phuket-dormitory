39ead0862224dd0ffccd102be7e1be636f490707
Fluke <jesadakorn.kirtnu@gmail.com>
2021-01-30 23:52:14 +0700

    abbYear Fix

--- frontend/pages/admin/payment/index.js
+++ frontend/pages/admin/payment/index.js
@@ -69,7 +69,7 @@ const Payment = () => {
                 <label htmlFor="abbYear">ปี</label>
                 <select
                     className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    name="year"
+                    name="abbYear"
                     ref={register}
                 >
                     {
